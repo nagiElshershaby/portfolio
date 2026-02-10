e33edf53c65d7bf7674f39abadd1f43aa59cfb73
nagiElshershaby <mohamednagi903@gmail.com>
2026-02-10 23:53:37 +0000

    added Harmoniq to the exp section

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3185104367"
+    serviceWorkerVersion: "4040948397"
   }
 });
