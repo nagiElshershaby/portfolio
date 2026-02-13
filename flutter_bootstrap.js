494d54e88c8465fbeade6ecb6d85f143cb0fd0ce
nagiElshershaby <mohamednagi903@gmail.com>
2026-02-13 02:35:23 +0000

    profile pic fix

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"3452d735bd38224ef2db85ca763d862d6326b1
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2654392724" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2469451539" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
