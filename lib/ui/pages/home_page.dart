bb3686000410b16e682b8bf577f11b76a42db478
nagielshershaby <mohamednagi903@gamil.com>
2026-02-13 04:33:19 +0200

    profile pic fix

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -232,7 +232,7 @@ class _ProfileAvatar extends StatelessWidget {
       backgroundColor: Theme.of(context).colorScheme.inverseSurface,
       child: ClipOval(
         child: Image.asset(
-          'assets/images/profile.jpeg',
+          'assets/images/profile/profile.jpeg',
           width: radius * 2,
           height: radius * 2,
           fit: BoxFit.cover,
